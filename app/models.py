fbb7914b210616591cd16719cfe4ac0d9154d96b
stanleyxisco <gistextra@gmail.com>
2026-06-21 11:55:49 -0700

    corrected CI/CD workflow file in the directory

--- app/models.py
+++ app/models.py
@@ -3,21 +3,23 @@ from sqlalchemy.sql import func
 from geoalchemy2 import Geometry
 from .database import Base
 
+
 class Vehicle(Base):
     __tablename__ = "vehicles"
 
-    id = Column(String, primary_key=True, index=True) # e.g., "vehicle_101"
+    id = Column(String, primary_key=True, index=True)  # e.g., "vehicle_101"
     driver_name = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
 class LocationLog(Base):
     __tablename__ = "location_logs"
 
     id = Column(Integer, primary_key=True, index=True)
     vehicle_id = Column(String, ForeignKey("vehicles.id"), index=True)
-    
+
     # PostGIS Geometry column representing a point (Longitude, Latitude)
     # srid=4326 is the spatial reference system identifier for standard global GPS coordinates
     coordinates = Column(Geometry(geometry_type='POINT', srid=4326), nullable=False)
-    
+
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
